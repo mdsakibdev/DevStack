ff4a7dc31a62eb4927c05e94d8b97b7cb8b8f4e6
Abdur Razzaq Sakib <abdurrazzaksakib2025@gmail.com>
2026-09-11 19:15:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from "./components/navbar/Navbar"
+
+
+function App() {
+
+  return (
+    <div className="font-inter">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logoImage from '../../assets/logo-text.png'
+import { HiOutlineMenu } from "react-icons/hi";
+
+function Navbar() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Navbar
